a647c8da37d8bee913eec40547e85583c2ff5ee6
Zsófia Kilyénfalvi <zsofikilyenfalv@gmail.com>
2022-09-30 21:24:42 +0200

    commit

---
+++ UM/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+		public Guid ID { get; set; } = Guid.NewGuid();
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+        public string FullName
+        {
+            get
+            {
+                return string.Format(
+                    "{0} {1}",
+                    LastName,
+                    FirstName);
+            }
+        }
+    }
+}
