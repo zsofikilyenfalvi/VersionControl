62b43cec22981686752b768319fb578bf86a0a21
Zsófia Kilyénfalvi <zsofikilyenfalv@gmail.com>
2022-10-07 09:56:10 +0200

    commit3

--- week4/week4/Form1.cs
+++ week4/week4/Form1.cs
@@ -12,9 +12,17 @@ namespace week4
 {
     public partial class Form1 : Form
     {
+        RealEstateEntities context = new RealEstateEntities();
+        List<Flat> Flats;
         public Form1()
         {
             InitializeComponent();
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            Flats = context.Flats.ToList();
         }
     }
 }
