ca25429344a07bb3476530168b162f3039e6c9bd
rd7z7p <rd7z7p@SZA0032037>
2022-10-21 14:24:22 +0200

    commit1

---
+++ week5/week5/DataRate1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+        public DateTime Date { get; set; }
+        public string Currency{ get; set; }
+        public decimal Value { get; set; }
+    }
+}
---
+++ week5/week5/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using week5.MnbServiceReference;
+
+namespace week5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            var mnbService = new MNBArfolyamServiceSoapClient();
+
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+
+            var response = mnbService.GetExchangeRates(request);
+
+            var result = response.GetExchangeRatesResult;
+
+            BindingList<DataRate1> rates = new BindingList<DataRate1>;
+
+        }
+    }
+}
