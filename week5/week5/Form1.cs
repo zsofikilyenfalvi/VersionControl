e1a84aa7a7afa9db842ec76c6b9f9051413fd821
rd7z7p <rd7z7p@SZA0032037>
2022-10-21 15:00:53 +0200

    commit-chart

--- week5/week5/Form1.cs
+++ week5/week5/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 using System.Xml;
 using week5.MnbServiceReference;
 
@@ -19,6 +20,7 @@ namespace week5
         public Form1()
         {
             InitializeComponent();
+            LoadChart();
 
             var mnbService = new MNBArfolyamServiceSoapClient();
 
@@ -52,7 +54,26 @@ namespace week5
                 var value = decimal.Parse(childElement.InnerText);
                 if (unit != 0)
                     rate.Value = value / unit;
-            }
+            }           
+        } 
+        
+        private void LoadChart()
+            {
+            chartRateData.DataSource = rates;
+
+            var series = chartRateData.Series[0];
+            series.ChartType = SeriesChartType.Line;
+            series.XValueMember = "Date";
+            series.YValueMembers = "Value";
+            series.BorderWidth = 2;
+
+            var legend = chartRateData.Legends[0];
+            legend.Enabled = false;
+
+            var chartArea = chartRateData.ChartAreas[0];
+            chartArea.AxisX.MajorGrid.Enabled = false;
+            chartArea.AxisY.MajorGrid.Enabled = false;
+            chartArea.AxisY.IsStartedFromZero = false;
         }
     }
 
