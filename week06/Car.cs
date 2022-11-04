12aac1847a821ae3d901d65e0bc0e8cc68841bd2
Zsófia Kilyénfalvi <zsofikilyenfalv@gmail.com>
2022-11-04 22:21:00 +0100

    Car

---
+++ week06/Car.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Car
+{
+    public class Car : Toy
+    {
+        protected override void DrawImage(Graphics g)
+        {
+            Image imageFile = Image.FromFile("Images/car.png");
+            g.DrawImage(imageFile, new Rectangle(0, 0, Width, Height));
+        }
+    }
+}
---
+++ week06/Toy.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using.System.Windows.Forms;
+
+public class Toy: Label
+{
+    protected override void DrawImage(Graphics g)
+    {
+        g.FillEllipse(new SolidBrush(Color.Blue), 0, 0, Width, Height);
+    }
+}
