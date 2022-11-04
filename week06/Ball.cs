260808bcdd8f77d5ff30d5854c3a02e0134ae5f7
Zsófia Kilyénfalvi <zsofikilyenfalv@gmail.com>
2022-11-04 22:00:39 +0100

    class

---
+++ week06/Ball.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using.System.Windows.Forms;
+
+
+public class Class1: Label
+{
+	public Class1()
+	{
+		AutoSize = false;
+		Width = 50;
+		Height = Width;
+		Paint += Ball_Paint;
+	}
+    private void Ball_Paint(object sender, PaintEventArgs e)
+    {
+        DrawImage(e.Graphics);
+    }
+
+    protected void DrawImage(Graphics g)
+    {
+        g.FillEllipse(new SolidBrush(Color.Blue), 0, 0, Width, Height);
+    }
+
+    public void MoveBall()
+    {
+        Left += 1;
+    }
+
+}
