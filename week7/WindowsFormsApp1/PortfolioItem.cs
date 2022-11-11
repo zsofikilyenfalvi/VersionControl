a45dc9b87f52a273ec176b6a2475492c30fe1682
rd7z7p <rd7z7p@SZA0032037>
2022-11-11 14:10:46 +0100

    Commit -Ticks+PortfolioItem

---
+++ week7/WindowsFormsApp1/PortfolioItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class PortfolioItem
+{
+	public string Index { get; set; }
+	public decimal Volume { get; set; }
+
+	public PortfolioItem()
+	{
+
+	}
+}
--- week7/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ week7/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -12,9 +12,13 @@ namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
+        PortfolioEntities context = new PortfolioEntities();
+        List<Tick> Ticks;
         public Form1()
         {
             InitializeComponent();
+            Ticks = context.Tick.ToList();
+            dataGridView1.DataSource = Ticks;
         }
     }
 }
