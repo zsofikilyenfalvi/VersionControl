51ac52c9fb0e9dd5d7c5dfce7fdf8c34bc07232a
rd7z7p <rd7z7p@SZA0032037>
2022-12-09 14:18:01 +0100

    commit 2

---
+++ week10/rd7z7p_week10/rd7z7p_week10/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WorldsHardestGame;
+
+namespace rd7z7p_week10
+{
+    public partial class Form1 : Form
+    {
+        GameController gc = new GameController();
+        GameArea ga;
+
+        int populationSize = 100;
+        int nbrOfSteps = 10;
+        int nbrOfStepsIncrement = 10;
+        int generation = 1;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            ga = gc.ActivateDisplay();
+            this.Controls.Add(ga);
+
+            gc.AddPlayer();
+            gc.Start(true);
+
+        }
+    }
+}
